d33910c3541c210abe2c3f9041b355a3f09cd252
Soniya Kambli <100508652+techsoniya@users.noreply.github.com>
2024-02-17 20:09:07 +0530

    Update authMiddleware.js

--- my-node-api/middleware/authMiddleware.js
+++ my-node-api/middleware/authMiddleware.js
@@ -1,8 +1,6 @@
-// middleware/authMiddleware.js
+
 const authMiddleware = (req, res, next) => {
-    // Implement your authentication logic here
-    // For example, check for a valid token or session
-    // If authentication fails, return a 401 Unauthorized response
+   
     next();
   };
   
